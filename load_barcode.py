534144a4366b4f2d2ab59b8e65e1825f1be43fe5
Tony Kim <wlwkgus@nate.com>
2016-12-15 12:45:26 +0900

    done

--- load_barcode.py
+++ load_barcode.py
@@ -7,13 +7,16 @@ if __name__ == '__main__':
     print('input image files are: ')
     print(api_client.load_image_names_from_directory())
     api_response = api_client.detect_text_from_directory()
+    f = open('result.csv', 'w')
     # print api_response
     for filename, blob in api_response.iteritems():
         barcode = barcode_finder.find_pin_number(blob)
         if barcode != 'ERROR_PIN_NOT_FOUND':
             print("{0} : {1}".format(filename, barcode))
+            f.write('{0}\n'.format(barcode))
         else:
             print("error")
+    f.close()
     # TODO : https://developers.google.com/identity/protocols/application-default-credentials
     # TODO : Check URL above, and export env var.
     # TODO : export GOOGLE_APPLICATION_CREDENTIALS= ~/elenakey.json
